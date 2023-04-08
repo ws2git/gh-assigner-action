163433e2d6df09dd99f23597f8661deda42c7fb6
Rodrigo Motta <55290205+rmottainfo@users.noreply.github.com>
2023-04-08 17:18:37 -0300

    This commit introduces a new GitHub Action to automatically assign the pull request (PR) creator as the assignee. This automation streamlines the workflow by ensuring the person who opens a PR is immediately responsible for it, improving visibility and accountability.
    This feature includes two main components:
    
    commit 153df3acf2d259a8198ce1717edee862b5cb9109
    Author: Rodrigo Motta <55290205+rmottainfo@users.noreply.github.com>
    Date:   Sat Apr 8 17:10:29 2023 -0300
    
        Create assign-pr.sh
    
        This commit introduces a new shell script, `assign-pr.sh`, designed to automatically assign the pull request (PR) creator as the assignee. This script is a core component of the `Auto Assign PR Creator` GitHub Action, leveraging the `gh` CLI to interact with the repository.
    
        **Key changes:**
    
        * **`assign-pr.sh`:** A new executable script is added. It takes the PR link and creator's username as arguments, then uses the `gh pr edit` command to add the user as an assignee to the specified PR.
    
        For more details on the GitHub CLI and its commands, refer to the official documentation:
    
        * [GitHub CLI Manual](https://cli.github.com/manual/)
    
    commit 7dbeb1e382a9b64fc60fce93f188959fba6ff413
    Author: Rodrigo Motta <55290205+rmottainfo@users.noreply.github.com>
    Date:   Sat Apr 8 17:05:54 2023 -0300
    
        Create action.yml
    
        Add workflow for auto-assigning PR creator
    
        This commit introduces a new GitHub Action to automatically assign the pull request (PR) creator as the assignee. This automation streamlines the workflow by ensuring that the person who opens a PR is immediately responsible for it, improving visibility and accountability.
    
        **Key changes:**
    
        * **`action.yml`:** A new metadata file is added to define the action's behavior. It specifies the inputs required (`pr_url` and `pr_creator`) and a composite run step that executes a shell script.
    
        This action follows the best practices for creating and publishing GitHub Actions. For more information on the syntax and structure of action metadata files, please refer to the official documentation:
    
        * [Metadata syntax for GitHub Actions](https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions)
        * [Using a composite run step](https://docs.github.com/en/actions/creating-actions/creating-a-composite-action)

---
+++ assign-pr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+PR_LINK="$1"
+PR_CREATOR="$2"
+
+if [[ -z "$PR_LINK" || -z "$PR_CREATOR" ]]; then
+  echo "Usage: $0 <pr_link> <pr_creator>"
+  exit 1
+fi
+
+echo "Assigning pull request creator '$PR_CREATOR' to PR: $PR_LINK"
+
+gh pr edit "$PR_LINK" --add-assignee "$PR_CREATOR"
